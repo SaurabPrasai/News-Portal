ad980bdb435af79d63ccaedaafd3af883c7bb32b
Saurab Prasai <saurablovetocode@gmail.com>
2025-01-29 19:21:20 +0545

    changes in port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const axios = require("axios");
 const cheerio = require("cheerio");
-const PORT = 3000;
+const PORT = process.env.PORT||8080;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
